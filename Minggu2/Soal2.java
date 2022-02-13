54afc98393262a70396c26093917b0a298ed12e0
Ghaniaja26 <95170644+Ghaniaja26@users.noreply.github.com>
2022-02-13 15:59:45 +0700

    Add files via upload

---
+++ Minggu2/Soal2.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package soal2;
+import java.util.Scanner;
+/**
+ *
+ * @author GHANI
+ */
+public class Soal2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        long angka;
+        int i;
+        for (i=0;i<6;i++){
+            try{
+            angka = input.nextLong();
+            System.out.println(+ angka +" can be filled in :");       
+                    if (angka >= -128 && angka <= 127) {
+                    System.out.println("*byte");
+                }
+                    if (angka >= -32768 && angka <= 32767){
+                    System.out.println("*short");
+                }
+                    if (angka >= -2147483648 && angka <= 2147483647){
+                    System.out.println("*integer");
+                }
+                    if(angka >=-9223372036854775807L && angka <=9223372036854775806L) {
+                    System.out.println("*long");
+                }
+            }catch (Exception caseError){
+                System.out.println(" cant be fit in anywhere.");
+                input.next();
+            }
+        }
+    }
+}
