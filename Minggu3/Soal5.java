2fd14a1a7e0ba21cebe6dfde623a8f46bf0dc539
Ghaniaja26 <95170644+Ghaniaja26@users.noreply.github.com>
2022-02-19 10:49:33 +0700

    Update Soal5.java

--- Minggu3/Soal5.java
+++ Minggu3/Soal5.java
@@ -5,22 +5,23 @@ import java.util.Scanner;
 /**
  *
  * @author GHANI
+ * Program ini bertujuan untuk menentukan apakah keempat mobil tersebut harus berhenti atau jalan berdasarkan plat nomornya 
  */
 public class Soal5 {
 
     public static void main(String[] args) {
        // Data Fields //
        Scanner keyboard = new Scanner (System.in);
-       String plat1;
-       String plat2;
-       String plat3;
-       String plat4;
-       String gabungan;
-       BigInteger angka; 
-       BigInteger pengurangan;
-       BigInteger pembagian;
-       BigInteger modulo;
-       BigInteger NOL = new BigInteger ("0");
+       String plat1; // plat nomor mobil 1
+       String plat2; // plat nomor mobil 2
+       String plat3; // plat nomor mobil 3
+       String plat4; // plat nomor mobil 4
+       String gabungan; // gabungan dari keempat plat nomor tersebut
+       BigInteger angka;  // konversi dari String gabungan plat nomor menjadi BigInteger
+       BigInteger pengurangan; // pengurangan big integer yaitu angka 999999
+       BigInteger pembagian; // pembagian big integer yaitu angka 5
+       BigInteger modulo; // modulo big integer yaitu angak 5
+       BigInteger NOL = new BigInteger ("0"); // Inisialisasi angka 0 dengan tipe BigInteger untuk keperluan kondisi apakah mobil-mobil tersebut jalan atau berhenti
        
        // Behavior //
        plat1 = keyboard.next();
@@ -43,3 +44,10 @@ public class Soal5 {
     }
     
 }
+
+// Lesson Learn //
+
+/* lesson learn yang saya dapatkan dalam mengerjakan soal ini adalah
+ * saya jadi lebih paham dan tau bagaimana cara menggunakan class BigInteger
+ * di bahasa java ini
+ */
