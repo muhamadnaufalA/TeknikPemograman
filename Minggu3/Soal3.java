80763b69951bdc41470fb6fee35197d06baa0293
Ghaniaja26 <95170644+Ghaniaja26@users.noreply.github.com>
2022-02-19 10:41:58 +0700

    Update Soal3.java

--- Minggu3/Soal3.java
+++ Minggu3/Soal3.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 /**
  *
  * @author GHANI
+ * Program ini bertujuan untuk menampilkan sebuah angka sesuai dengan input angka dan operasi yang diinputkan
  */
 public class Soal3 {
 
     public static void main(String[] args) {
         // kamus data //
         Scanner keyboard = new Scanner(System.in);
-        int angka1,angka2;
-        char operator;
+        int angka1,angka2; // angka yang akan diinput dan dioperasikan
+        char operator; // operator yang akan diinputkan
         
         // algoritma //
         angka1 = keyboard.nextInt();
-        operator = keyboard.next().charAt(0);
+        operator = keyboard.next().charAt(0); // input operator
         angka2 = keyboard.nextInt();
         if ((angka1>=1 && angka1<=1000) && (angka2>=1 && angka2<=1000)){
             switch (operator){
@@ -29,3 +30,11 @@ public class Soal3 {
     }
     
 }
+
+// Lesson Learn //
+
+/* Lesson learn yang saya dapatkan dalam mengerjakan soal ini adalah
+ * saya jadi tau bagaimana cara menginput sebuah karakter pada bahasa java
+ * karena pada bahasa java tidak ada perintah nextChar, maka perintah tersebut
+ * diubah menjadi next().charAt(0).
+ */
