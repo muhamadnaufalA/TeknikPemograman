4d2f2a0043a6a73f1ff427515bbb64cc861c8238
Ghaniaja26 <95170644+Ghaniaja26@users.noreply.github.com>
2022-04-06 19:02:56 +0700

    Add files via upload

---
+++ Minggu7/ATMMain.java
@@ -0,0 +1,14 @@
+package minggu7;
+
+/**
+ *
+ * @author GHANI
+ */
+public class ATMMain {
+   // main method creates and runs the ATM
+   public static void main(String[] args) {
+      ATM theATM = new ATM();    
+      theATM.run();
+   }
+} 
+
