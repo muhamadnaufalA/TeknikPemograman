a0b3a88d65c98376f1c8b5542ed3b74d49af50e8
Ghaniaja26 <95170644+Ghaniaja26@users.noreply.github.com>
2022-04-17 21:58:15 +0700

    upload Praktikum Tekprog W10 (Kelompok 10)

---
+++ Minggu10/src/main/java/com/crud/generic/entity/baseEntity/BaseEntity.java
@@ -0,0 +1,37 @@
+package com.crud.generic.entity.baseEntity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int no;
+	
+	private String name;	
+
+	public BaseEntity() {
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+}
