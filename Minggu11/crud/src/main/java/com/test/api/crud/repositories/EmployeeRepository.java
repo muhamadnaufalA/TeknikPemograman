c77050afc8a709c28cd6ada479be6f2a49a892a3
Ghaniaja26 <95170644+Ghaniaja26@users.noreply.github.com>
2022-04-24 21:16:23 +0700

    Delete Minggu11/crud directory

--- Minggu11/crud/src/main/java/com/test/api/crud/repositories/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.test.api.crud.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.test.api.crud.models.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
