99ec88ed2ef68d2b5805f006bfb85ed100352c97
Ghaniaja26 <95170644+Ghaniaja26@users.noreply.github.com>
2022-04-24 21:15:13 +0700

    Add files via upload

---
+++ Minggu11/crud/src/main/java/com/test/api/crud/repositories/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.test.api.crud.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.test.api.crud.models.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
